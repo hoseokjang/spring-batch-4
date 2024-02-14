383f286a002519a1199d789d622e17dcde6068a5
hoseokjang <hoseok210@gmail.com>
2024-02-14 16:00:59 +0900

    init commit

---
+++ src/main/java/com/batchStudy/batchStudy/BatchStudyApplication.java
@@ -0,0 +1,15 @@
+package com.batchStudy.batchStudy;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class BatchStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchStudyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/batchStudy/batchStudy/SimpleJonConfiguration.java
@@ -0,0 +1,41 @@
+package com.batchStudy.batchStudy;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class SimpleJonConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job simpleJob() {
+        return jobBuilderFactory.get("simpleJob")
+                .start(simpleStep1())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep1() {
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet((contribution, chunContext) -> {
+                    log.info(">>> This is Step1");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+
+}
