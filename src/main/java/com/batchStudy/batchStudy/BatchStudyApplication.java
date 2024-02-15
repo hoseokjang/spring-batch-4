5c289f2e0e1b35828b34a5d725086516ccdcd389
hoseokjang <hoseok210@gmail.com>
2024-02-15 11:47:10 +0900

    job scheduler 추가

--- src/main/java/com/batchStudy/batchStudy/BatchStudyApplication.java
+++ src/main/java/com/batchStudy/batchStudy/BatchStudyApplication.java
@@ -3,9 +3,11 @@ package com.batchStudy.batchStudy;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableBatchProcessing
+@EnableScheduling
 public class BatchStudyApplication {
 
 	public static void main(String[] args) {
